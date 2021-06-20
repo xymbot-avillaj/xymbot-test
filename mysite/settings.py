39caed1d0d02d6245c675ab420fd72958bedb328
xymbot-avillaj <85871913+xymbot-avillaj@users.noreply.github.com>
2021-06-20 22:04:08 +0200

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -74,13 +74,14 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
-DATABASES = {
+"""
+DATABASES = {    
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }    
 }
+"""
 
 
 # Password validation
